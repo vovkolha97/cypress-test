1525a9c936e48245f9e24cecd28869c537339832
vovkolha97 <vovkolha97@gmail.com>
2022-06-13 22:14:32 +0300

    update files

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,40 @@
-describe('first spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.saucedemo.com/')
+const goHome = () => {cy.visit('https://www.saucedemo.com/')}
+const loginAs = (username) => {
+  cy.get('[data-test="username"]')
+  .type(username)
+  .should("have.value", username);
+
+  cy.get('[data-test="password"]')
+  .type('secret_sauce')
+  .should("have.value", "secret_sauce");
+
+  cy.get('[data-test="login-button"]').click();
+  // check if logout link present
+}
+const logout = () => {
+  cy.get('#react-burger-menu-btn').click();
+    cy.get('#logout_sidebar_link').click();
+}
+
+describe('add items to cart', () => {
+  it('login, add items, logout', () => {
+    goHome();
+    loginAs('standard_user');
+    cy.url().should('include', '/inventory');
+ 
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('#shopping_cart_container').click();
+    cy.get('[data-test="continue-shopping"]').click();
+    cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    cy.get('#shopping_cart_container').click();
+
+    cy.get('[data-test="continue-shopping"]').click();
+    logout();
+  
+  })
+   
+  it('login as locked_user', () =>{
+    loginAs('locked_out_user');
+    cy.get('[data-test="error"]');
   })
 })
