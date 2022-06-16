51b9c7f3ea9db0765a15f00d074c852f9d1ce2b6
vovkolha97 <vovkolha97@gmail.com>
2022-06-17 00:45:52 +0300

    fixtures added, flow extended

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,12 @@
+beforeEach(function () {
+  cy.fixture('exemple').then(function(data) {
+    this.data = data;
+    cy.log('Data: ', this.data)
+  })
+}) 
+
 const goHome = () => {
   cy.visit('https://www.saucedemo.com/')
-}
-const loginAs = (username) => {
-  cy.get('[data-test="username"]')
-  .type(username)
-  .should("have.value", username);
-
-  cy.get('[data-test="password"]')
-  .type('secret_sauce')
-  .should("have.value", "secret_sauce");
-
-  cy.get('[data-test="login-button"]').click();
 }
 
 const openShoppingCart = () => {
@@ -18,46 +14,64 @@ const openShoppingCart = () => {
   cy.url().should('include', '/cart');
 }
 
-const openArticlePanel = () => {
-  cy.get('[data-test="continue-shopping"]').click();
-  cy.url().should('include', '/inventory');
-}
-
 const logout = () => {
   cy.get('#react-burger-menu-btn').click();
-    cy.get('#logout_sidebar_link').click();
-    cy.get('[data-test="login-button"]');
-}
+  cy.get('#logout_sidebar_link').click();
+  cy.get('[data-test="login-button"]');
+} 
 
-describe('add items to cart', () => {
-  it('login, add items, logout', () => {
+describe('test set for web automation', () => {
+
+  it('login as standerd_user, add items, logout', function () {
+
     goHome();
-    loginAs('standard_user');
+
+    cy.get('[data-test="username"]').type(this.data.standardUser_login);
+    cy.get('[data-test="password"]').type(this.data.password);
+  
+    cy.get('[data-test="login-button"]').click();
     cy.url().should('include', '/inventory');
 
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get('.inventory_item .inventory_item_price:eq(0)')
     cy.get('.shopping_cart_badge');
     cy.get('[data-test="remove-sauce-labs-backpack"]');
 
     openShoppingCart();
+    cy.get('.cart_item .inventory_item_price:eq(0)')
     //add checking of total amount
-
-    openArticlePanel();
+    cy.get('[data-test="continue-shopping"]').click()
+  
 
     cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    cy.get('.inventory_item .inventory_item_price:eq(3)')
     cy.get('.shopping_cart_badge');
     cy.get('[data-test="remove-sauce-labs-bolt-t-shirt"]');
 
     openShoppingCart();
+    cy.get('.cart_item .inventory_item_price:eq(1)')
     //add checking of total amount
 
-    openArticlePanel();
+    cy.get('[data-test="checkout"]').click();
+    cy.url().should('include', '/checkout-step-one')
+
+    cy.get('[data-test="firstName"]').type(this.data.Name)
+    cy.get('[data-test="lastName"]').type(this.data.LastName)
+    cy.get('[data-test="postalCode"]').type(this.data.ZipCode)
+
+    cy.get('[data-test="continue"]').click()
+    cy.url().should('include', '/checkout-step-two')
+    //add checking of total amount
+    cy.get('[data-test="finish"]').click()
+    cy.url().should('include', '/checkout-complete')
+
     logout();
-  
   })
    
-  it('login as locked_user', () =>{
-    loginAs('locked_out_user');
+  it('login as locked_user', function () {
+    cy.get('[data-test="username"]').type(this.data.lockedUser_login);
+    cy.get('[data-test="password"]').type(this.data.password);
+    cy.get('[data-test="login-button"]').click();
     cy.get('[data-test="error"]');
-  })
+  }) 
 })
